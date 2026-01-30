fd98cca80cbf103e1d2ed520ba6c88ea54d0619c
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2026-01-30 11:37:05 -0500

    I added code to figureRoutes.js

---
+++ server/routes/api/figureRoutes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const {
+    searcgFigures,
+    getFigureById,
+    createFigure,
+    getPopularFigures 
+} = require('../controllers/figureController');
+const { protect, authorize } = require('../middleware/auth');
+
+
+// @route GET /api/figures/search
+// @desc Search historical figures with filters
+// @ access Public
+router.get('/search', searcgFigures);
+
+// @route GET /api/figures/popular
+// @desc Get yop 10 most viewed figures
+// @access Public
+router.get('/popular', getPopularFigures);
+
+// @route GET /api/figures/:id
+// @desc Get single figure by ID
+// @access Public 
+router.get('/:id', getFigureById);
+
+
+// @route POST /api/figures
+// @desc Create new historical figure
+// @access Private/Admin
+router.post('/', protect, authorize('admin'), createFigure);
+
+
+
+
+module.exports = router;
